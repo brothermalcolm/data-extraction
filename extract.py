a59ac3f6a5949e86f932e48dbc193ac61ac9db1e
Malcolm Ng <malcolmng@alumni.purdue.edu>
2017-05-09 20:57:56 +0800

    Update extract.py
    # Read file line by line

--- extract.py
+++ extract.py
@@ -1,1 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 12 12:14:22 2017
 
+@author: SERNMO
+"""
+
+#%% 
+# Read file line by line
+fhand = open('16112601.txt')
+for line in fhand:
+    print(line.rstrip().upper())
